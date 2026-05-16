6339e47d867b11c5d6a10756a482227542587e76
shanmithasatram-ctrl <shanmithasatram@gmail.com>
2026-05-16 16:09:03 +0530

    Add files via upload

--- logger.js
+++ logger.js
@@ -1,8 +1,1 @@
-const fs = require('fs');
-
-function logger(message) {
-    const log = `${new Date().toISOString()} - ${message}\n`;
-    fs.appendFileSync('app.log', log);
-}
-
-module.exports = logger;
+// Backend logger middleware
